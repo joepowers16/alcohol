2fd2e9f5aad2ec8bc0ae535e27ce1984e4442056
Joe Powers <joepowers16@gmail.com>
2019-10-17 13:15:58 -0700

    updates setup.R to use install.packages()

--- setup.R
+++ setup.R
@@ -2,43 +2,16 @@
 ## Project: Humane Workflows
 ## Script purpose: Load required packages
 ## Date: 2019-10-16
-## Author: Joseph T. Powers (copied from H. Wickham's Tidy Tools workshop)
+## Author: Joseph T. Powers 
 ##############################################################################-
 
 # Please execute this script to ensure you have all the packages you need for 
 # this demo 
 
-please_install <- function(pkgs, install_fun = install.packages) {
-  if (length(pkgs) == 0) {
-    return(invisible())
-  }
-  if (!interactive()) {
-    stop("Please run in interactive session", call. = FALSE)
-  }
-  
-  title <- paste0(
-    "Ok to install these packges?\n",
-    paste("* ", pkgs, collapse = "\n")
-  )
-  ok <- menu(c("Yes", "No"), title = title) == 1
-  
-  if (!ok) {
-    return(invisible())
-  }
-  
-  install_fun(pkgs)
-}
-
-# Do you have all the needed packages? ------------------------------------
-
-tidytools <- 
+install.packages(
   c("devtools", "roxygen2", "usethis", "testthat", "tidyverse", "magrittr", 
-    "knitr", "kableExtra", "scales", "here", "fs")
-
-have <- rownames(installed.packages())
-needed <- setdiff(tidytools, have)
-
-please_install(needed)
+    "knitr", "kableExtra", "scales", "here", "fs", "glue")
+  )
 
 devtools::install_github("joepowers16/breadcrumbs")
 
